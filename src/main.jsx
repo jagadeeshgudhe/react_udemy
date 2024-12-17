b8ea19350dda187c8861bb5af95b8cb081b9b02b
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2024-12-17 23:59:35 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const container = document.getElementById('root');
+const container = document.getElementById('jagadeesh');
 const root = ReactDOM.createRoot(container);
 root.render(<p>Hello</p>);
