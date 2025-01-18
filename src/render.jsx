90e4317317bdba13b5ebe9a46d93bdd23b4cc148
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2025-01-18 22:50:08 +0530

    updated

---
+++ src/render.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+const myelement=(
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <th>Age</th>
+        </tr>
+        <tr>
+            <th>Place</th>
+        </tr>
+    </table>
+);
+const container=document.getElementById('root');
+const root=ReactDom.createRoot(container);
+root.render(myelement);
